399e61a0e42096fac7014780ed73c95e096acdbd
Fernando Basso <fernandobasso.br@gmail.com>
2022-08-12 06:42:57 -0300

    Add articles scaffold

---
+++ db/migrate/20220812092510_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title, limit: 128
+      t.text :body
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
