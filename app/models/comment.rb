3a8bfcc0d0576d4fb34354f1ac0270604e55d467
Fernando Basso <fernandobasso.br@gmail.com>
2022-08-14 09:19:28 -0300

    Add TODO send email to article author callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,9 +3,15 @@ class Comment < ApplicationRecord
   validates :name, :email, :body, presence: true
   validate :article_should_be_published
 
+  after_create :email_article_author
+
   def article_should_be_published
     if article && !article.published?
-      errors.add(:article_id, 'is not published yet') 
+      errors.add(:article_id, 'is not published yet')
     end
+  end
+
+  def email_article_author
+    p "TODO: Email -> #{article.user.email} replied in your article."
   end
 end
