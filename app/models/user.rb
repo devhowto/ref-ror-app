cb78eecc107bc2e77f5b532e86ca79d62db5402f
Fernando Basso <fernandobasso.br@gmail.com>
2022-08-14 07:04:30 -0300

    Add a few validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ApplicationRecord
+  validates :email, 
+    uniqueness: true,
+    length: {
+      within: 5..50,
+      message: 'Beteween 5 and 50 please'
+    },
+    format: {
+      with: /\A[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}\z/i,
+      message: 'This is not an email address'
+     }
+
+  validates :password,
+     confirmation: true
+
   has_one :profile, dependent: :destroy
   has_many :articles,
     -> { order 'published_at DESC, title ASC' },
