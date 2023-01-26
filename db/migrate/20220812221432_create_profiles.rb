f54db4e68b356e7ffeb198e26bf1c14cefaa142e
Fernando Basso <fernandobasso.br@gmail.com>
2023-01-26 07:12:38 -0300

    Better document profiles migration

--- db/migrate/20220812221432_create_profiles.rb
+++ db/migrate/20220812221432_create_profiles.rb
@@ -1,7 +1,11 @@
 class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
+      ##
+      # This creates `user_id` FK.
+      #
       t.references :user, null: false, foreign_key: true
+
       t.string :name, limit: 96
       t.date :birthday
       t.text :bio
