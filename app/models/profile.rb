ef72d97b98502405884b3ff96acf2c27a891ce5b
Fernando Basso <fernandobasso.br@gmail.com>
2023-01-28 07:47:45 -0300

    Better document profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,7 @@
 class Profile < ApplicationRecord
+  ##
+  # We could also add `optional: true`, but for our purposes, we don't
+  # want a profile to exist without a user.
+  #
   belongs_to :user
 end
-
