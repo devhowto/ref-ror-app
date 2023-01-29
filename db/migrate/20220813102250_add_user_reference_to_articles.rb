ffe1621b5cc79f2e73fbb7bc813fe43e5a385a09
Fernando Basso <fernandobasso.br@gmail.com>
2023-01-29 12:08:29 -0300

    Add some doc comments to a few migrations

--- db/migrate/20220813102250_add_user_reference_to_articles.rb
+++ db/migrate/20220813102250_add_user_reference_to_articles.rb
@@ -1,5 +1,8 @@
 class AddUserReferenceToArticles < ActiveRecord::Migration[7.0]
   def change
+    ##
+    # Adds `user_id` to `articles`.
+    #
     add_reference :articles, :user, foreign_key: true
   end
 end
--- db/migrate/20220813131744_create_join_table_articles_categories.rb
+++ db/migrate/20220813131744_create_join_table_articles_categories.rb
@@ -1,8 +1,12 @@
 class CreateJoinTableArticlesCategories < ActiveRecord::Migration[7.0]
   def change
     create_join_table :articles, :categories do |t|
-      t.index [:article_id, :category_id]
-      t.index [:category_id, :article_id]
+      #
+      # NOTE: A join tables does not need a PK.
+      #
+
+      t.index %i[article_id category_id]
+      t.index %i[category_id article_id]
     end
   end
 end
