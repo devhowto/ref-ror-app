9180927ecd2891b448afc7e6b7912f0b5d6f27e3
Fernando Basso <fernandobasso.br@gmail.com>
2023-01-30 08:15:17 -0300

    Seed a few articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
+# This file should contain all the record creation needed to seed the
+# database with its default values. The data can then be loaded with the
+# bin/rails `db:seed`  command (or created alongside the database with
+# db:setup).
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
-User.create email: 'yoda@dev.io', password: '1234'
+user1 = User.create email: 'yoda@dev.io', password: '1234'
 
-Category.create [
-  { name: 'Programming' },
-  { name: 'Command Line'},
-  { name: 'Haskell' },
-]
+Category.create(
+  [
+    {
+      id: 1,
+      name: 'Programming'
+    },
+    {
+      id: 2,
+      name: 'Command Line'
+    },
+    {
+      id: 3,
+      name: 'Haskell'
+    }
+  ]
+)
+
+category1 = Category.find(1)
+category2 = Category.find(2)
+category3 = Category.find(3)
+
+Article.create(
+  [
+    {
+      id: 1,
+      title: 'Functional Programming',
+      body: 'The joys of ...',
+      user: user1,
+      categories: [category1, category3]
+    },
+    {
+      id: 2,
+      title: 'PureScript for the grown-ups',
+      body: 'PureScript was created by...',
+      user: user1,
+      categories: [category2]
+    },
+    {
+      id: 3,
+      title: 'Bash tips and tricks',
+      body: 'What about we investigate cool, little known bash stuff‽',
+      user: user1,
+      categories: [category1, category3]
+    }
+  ]
+)
